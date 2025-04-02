8d1088f8671b583796c25a7bc15c470f1ff0f1b6
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-02 08:07:17 -0300

    UserResource created for web connection

---
+++ src/main/java/com/jceco/course/resource/UserResource.java
@@ -0,0 +1,21 @@
+package com.jceco.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jceco.course.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Jeter123", "jeter@gmail.com", "47 998250912", "senha123");
+		return ResponseEntity.ok().body(u);
+	}
+}
