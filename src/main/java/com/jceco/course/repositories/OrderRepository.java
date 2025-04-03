d0f990981a3af502e338204ce9e2336a85ad1956
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-03 09:22:41 -0300

    OrderRepository created

---
+++ src/main/java/com/jceco/course/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.jceco.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jceco.course.entities.User;
+
+
+public interface OrderRepository extends JpaRepository<User, Long>{
+	
+	
+	
+}
