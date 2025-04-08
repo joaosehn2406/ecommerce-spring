f2e70dfb057713e8cd432adb2d07352d12295774
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-08 13:40:39 -0300

    Improvents for Exception Handling - findById

--- src/main/java/com/jceco/course/services/UserService.java
+++ src/main/java/com/jceco/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.jceco.course.entities.User;
 import com.jceco.course.repositories.UserRepository;
+import com.jceco.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -24,7 +25,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
