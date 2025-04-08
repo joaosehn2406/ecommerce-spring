7eebe5556bcca88bce00501f96e1be0a4effc9a3
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-08 09:17:24 -0300

    Payment class created, one-to-one association

--- src/main/java/com/jceco/course/config/TestConfig.java
+++ src/main/java/com/jceco/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jceco.course.entities.Category;
 import com.jceco.course.entities.Order;
 import com.jceco.course.entities.OrderItem;
+import com.jceco.course.entities.Payment;
 import com.jceco.course.entities.Product;
 import com.jceco.course.entities.User;
 import com.jceco.course.entities.enums.OrderStatus;
@@ -82,7 +83,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
 
+		orderRepository.save(o1);
 		
 	}
 	
